4ed5f4f78fcf6e08c29f06cd1d8980e4084d2912
Laura Gehrke <76006992+lauraghrk@users.noreply.github.com>
2022-10-10 20:19:55 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CardReceita from './components/CardReceita/CardReceita'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Vou ter que cozinhar</h1>
+      <CardReceita />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CardReceita/CardReceita.jsx
@@ -0,0 +1,13 @@
+import style from './CardReceita.css'
+
+function CardReceita() {
+    return (
+        <div className="card">
+            <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="foto da receita" />
+            <h3>Receita</h3>
+            <p>descrição.................</p>
+        </div>
+        
+    )
+}
+export default CardReceita
